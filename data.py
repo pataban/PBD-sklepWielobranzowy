439e00733c7cd9a9833aee3c268ef6f04812e7f2
pataban <brajerpatryk@gmail.com>
2021-12-01 21:41:44 +0100

    basic gui

---
+++ data.py
@@ -0,0 +1,17 @@
+
+def startDB():
+    print("start")
+
+def login(login,password):
+    print(login+" "+password)
+    return True
+
+def getData():
+    return
+
+
+
+if __name__=="__main__":
+    print("main")
+
+
